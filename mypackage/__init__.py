c0cae3dc11e5e3eb6d46d3d586a249960a105753
Aisling Dailey <aisling.dailey@protonmail.com>
2020-08-22 18:00:55 +0200

    Update __init__.py

--- mypackage/__init__.py
+++ mypackage/__init__.py
@@ -1,1 +1,19 @@
-from .core import *
+__all__ = [
+    'get_version'
+]
+
+from typing import Tuple as __Tuple
+
+from mypackage.__version__ import __author__
+from mypackage.__version__ import __author_email__
+from mypackage.__version__ import __license__
+from mypackage.__version__ import __version__
+from mypackage.__version__ import VERSION as __VERSION
+
+from mypackage import models
+#from mypackage._mypackage import my_function
+
+
+
+def get_version() -> __Tuple[int]:
+    return __VERSION
